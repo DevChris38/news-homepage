534adba061ffdb2adfbf9f84c306004aba7a0bb8
DevChris38 <124736625+DevChris38@users.noreply.github.com>
2023-02-17 08:36:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,16 +3,19 @@
     let nav = document.getElementById('nav');
     let ouvrir = document.getElementById('icon-menu');
     let fermer = document.getElementById('icon-menu-croix');
+    let griser = document.getElementById('griser');
 
       bouton.onclick = function(e){
         if(nav.style.display=="block"){
             nav.style.display="none";
             fermer.style.display="none";
+            griser.style.display="none";
             ouvrir.style.display="block";
         }else if(window.innerWidth < 1110){
             nav.style.display="block";
             ouvrir.style.display="none";
             fermer.style.display="block";
+            griser.style.display="block";
         }else{
             nav.style.display="flex";
         }
